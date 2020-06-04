09c6cb7172762c1dbc55c02ab4e02d134b5e3152
Шило Виктор Сергеевич <Shilo.VS@partner.ru>
2020-06-04 10:08:32 +1000

    Добавил валидацию введенного текста по количеству символов

--- lib/screens/list_of_tasks_screen.dart
+++ lib/screens/list_of_tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'detail_screen.dart';
 import 'package:todoapp/model/task.dart';
 
@@ -14,25 +15,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List<Task> taskList = <Task>[
-    Task(title: 'To do 1', isDone: false),
-    Task(
-        title: 'You should use CrossAxisAlignment.baseline if you.111',
-        isDone: false),
-    Task(
-        title:
-            'You should use CrossAxisAlignment.baseline if you.111You should use CrossAxisAlignment.baseline if you.111You should use CrossAxisAlignment.baseline if you.111e if you.111You should use CrossAxisAlignment.baseline if you.111',
-        isDone: false),
-    Task(title: 'To do 2', isDone: false),
-    Task(title: 'To do 3', isDone: false),
-    Task(title: 'To do 4', isDone: false),
-    Task(title: 'To do 5', isDone: false),
-    Task(title: 'To do 6', isDone: false),
-    Task(title: 'To do 7', isDone: false),
-    Task(title: 'To do 8', isDone: false),
   ];
 
   bool checkBoxValue = false;
+  bool validate = false;
+  final formKey = GlobalKey<FormState>();
   final _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   void selectedItem(String value) {
     switch (value) {
@@ -46,48 +40,69 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
    void deleteItem(int index) {
-    setState(() {
-      taskList.removeAt(index);
-    });
-    print('Delete');
-  }
+     setState(() {
+       taskList.removeAt(index);
+     });
+     print('Delete');
+     print(taskList);
+   }
 
   void addItem(String title) {
     setState(() {
       taskList.add(Task(title: title, isDone: false));
       _textEditingController.text = '';
+      print(taskList);
     });
   }
 
-   displayDialog(BuildContext context) async {
+    displayDialog(BuildContext context) {
     return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Создать задачу'),
-          content: TextField(
-            decoration: InputDecoration(
-              hintText: 'Введите название задачи',
-            ),
-            controller: _textEditingController,
+        context: context,
+        builder: (context) {
+          return buildAlertDialog(context);
+        }
+    );
+  }
+
+  Widget buildAlertDialog(BuildContext context) {
+    return AlertDialog(
+      title: Text('Создать задачу'),
+      content: Form(
+        key: formKey,
+        child: TextFormField(
+          maxLines: null,
+          controller: _textEditingController,
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Введите текст';
+            } else if (value.length > 50) {
+              return 'Максимальное число символов - 50';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            hintText: "Введите название задачи"
           ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Отмена'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            FlatButton(
-              child: Text('Добавить'),
-              onPressed: () {
-                addItem(_textEditingController.text);
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-    }
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Отмена'),
+          onPressed: () {
+            _textEditingController.text = '';
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('Добавить'),
+          onPressed: () {
+            if (formKey.currentState.validate()) {
+              addItem(_textEditingController.text);
+              Navigator.of(context).pop();
+            }
+          },
+        )
+      ],
     );
   }
 
@@ -121,57 +136,61 @@ class _MyHomePageState extends State<MyHomePage> {
         body: ListView.builder(
           itemCount: taskList.length,
           itemBuilder: (BuildContext context, int index) {
-            return InkWell(
-              onTap: () {
-                print('Tap!');
-                Navigator.push(context,
-                    MaterialPageRoute(
-                        builder: (context) => DetailScreen(title: taskList[index].title)
-                    )
-                );
-              },
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                padding: EdgeInsets.only(right: 8, top: 8, bottom: 8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Checkbox(
-                      value: taskList[index].isDone,
-                      onChanged: (newValue) {
-                        setState(() {
-                           taskList[index].isDone = newValue;
-                        });
-                      },
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${taskList[index].title}',
-                        textDirection: TextDirection.ltr,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 6,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 40,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(0),
-                        onPressed: () => deleteItem(index),
-                        child: Icon(
-                          Icons.delete,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
+            return buildListViewItem(context, index);
           },
         ));
+  }
+
+  Widget buildListViewItem(BuildContext context, int index) {
+    return InkWell(
+      onTap: () {
+        print('Tap!');
+        Navigator.push(context,
+            MaterialPageRoute(
+                builder: (context) => DetailScreen(title: taskList[index].title)
+            )
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        padding: EdgeInsets.only(right: 8, top: 8, bottom: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Checkbox(
+              value: taskList[index].isDone,
+              onChanged: (newValue) {
+                setState(() {
+                  taskList[index].isDone = newValue;
+                });
+              },
+            ),
+            Expanded(
+              child: Text(
+                '${taskList[index].title}',
+                textDirection: TextDirection.ltr,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 6,
+              ),
+            ),
+            SizedBox(
+              width: 40,
+              child: FlatButton(
+                padding: EdgeInsets.all(0),
+                onPressed: () => deleteItem(index),
+                child: Icon(
+                  Icons.delete,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
